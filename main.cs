71cc6ebdfd8d5a21faddd5859081f5794b25315b
Oscar Michel Michel <osambelacolareta@gmail.com>
2020-11-26 05:21:22 +0000

    add code

--- main.cs
+++ main.cs
@@ -18,15 +18,22 @@ public class Program
 
     //ejemplo 2  
     LinkedList<int> numeros = new LinkedList<int>();
+            //traer un nodo de otro array y empatarlo al array actual 
+            LinkedListNode<int> nodo = new LinkedListNode<int>(20); 
+            numeros.AddFirst(nodo);
+    
     //creamos un arrar dentro del linkelist
     foreach(int numero in new int[]{10,8,6,4,2,0}){
       //este metodo permite posicionar el ultimo nodo del array al inicio
-      numeros.AddFirst(numero);
+      //numeros.AddFirst(numero);
       //este metodo permite posicionar el primer nodo del array al inicio
       numeros.AddLast(numero);
+      //numeros.Remove(0);
     }
+    numeros.AddFirst(14);//añadir un nuevo nodo
     foreach(int numero in numeros){
       Console.WriteLine(numero);
     }
+    
   }
 }
